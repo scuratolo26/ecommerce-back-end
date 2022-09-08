cf7d1362d42703ec5f2209ce3bc01a7f1e4d99d4
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-09-07 22:23:57 -0400

    added database models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,30 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataType.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataType.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataType.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
